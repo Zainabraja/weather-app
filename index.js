8143c15c7d05c0bd0d0673292a1275305bfada74
Zainabraja <zainabraja53@gmail.com>
2022-03-06 22:26:39 +0530

    Deploy

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000!")
+app.listen((process.env.PORT || 3000), () => {
+    console.log("Server started!")
 });
 
 app.get('/', (req, res) => {
